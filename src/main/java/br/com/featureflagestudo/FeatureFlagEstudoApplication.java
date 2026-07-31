437390fbd2d2b16ab24ae260e0c59a8be1a5ff5c
Allan Sérgio <allansergio.dev@gmail.com>
2026-07-30 23:14:56 -0300

    initial commit

---
+++ src/main/java/br/com/featureflagestudo/FeatureFlagEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.featureflagestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagEstudoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureFlagEstudoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/featureflagestudo/FeatureFlagEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.featureflagestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagEstudoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
